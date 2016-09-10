797b6613f28cde82772fb208df8c6ac089a2755a
Dominik Sander <git@dsander.de>
2016-09-10 22:10:54 +0200

    Add OmniauthCallbacksController specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,6 +42,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
+  config.include ControllerMacros, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
