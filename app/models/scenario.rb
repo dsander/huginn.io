834b39c34615dacd37c6a6d9cdff194aee9a7340
Paul Platenius <Platte79@gmx.net>
2016-09-30 20:54:38 +0200

    validate presence of name, guid, agents in Scenarios. Validate presence of name and type in Scenario Agents

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -9,9 +9,20 @@ class Scenario < ApplicationRecord
   def agents_json_valid?
     begin
       self.data = JSON.parse(data) unless data.blank?
-      return true
+      validate_scenario
+      validate_scenario_agents
     rescue JSON::ParserError
       errors.add(:data, "not in JSON format")
+    end
+  end
+
+  def validate_scenario
+    errors.add(:base, "The provided data does not appear to be a valid Scenario.") if (%w[name guid agents] - data.keys).length > 0
+  end
+
+  def validate_scenario_agents
+    data["agents"].to_a.each do |agent|
+      errors.add(:base, "The Scenario contains invalid agents.") if agent["name"].blank? || agent["type"].blank?
     end
   end
 end
