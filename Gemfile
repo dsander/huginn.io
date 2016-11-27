0594c7d824e3a2c16d7d8cdc7d32520de5623a05
Dominik Sander <git@dsander.de>
2016-11-27 17:03:04 +0100

    Move agent updater files into separate repository

--- Gemfile
+++ Gemfile
@@ -23,8 +23,6 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'devise', '~> 4.2.0'
 gem 'bootstrap_form', '~> 2.5.0'
 gem 'omniauth-github', '~> 1.1.2'
-gem 'octokit', '~> 4.3.0'
-gem 'parser', '~> 2.3.1.2'
 gem 'kramdown', '~> 1.5.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,9 @@
 #
 
 require 'agent_importer'
-require 'agent_fetcher'
 require 'open-uri'
 
 if ENV['AGENT_JSON']
   data = JSON.parse(open(ENV['AGENT_JSON']).read)
   AgentImporter.run(data)
-else
-  data = AgentFetcher.run
-  File.open('agents.json', 'w').write(JSON.dump(data))
-  AgentImporter.run(data.map(&:stringify_keys))
 end
